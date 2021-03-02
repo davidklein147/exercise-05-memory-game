54ac79cdba7795c903ec98907f57017c7cdd0617
davidklein147 <davidklein147@gmail.com>
2021-03-02 18:12:32 +0200

    02/03/21

--- memory-game.js
+++ memory-game.js
@@ -13,9 +13,17 @@ let card1;
 let card2;
 let finish = 0;
 let steps = 0;
+let ouers = 0;
+let minit = 0;
+let secnce = 0;
+document.getElementById("time").innerHTML = `${ouers}:${minit}${secnce} <span  class="material-icons m-2" style="font-size: medium; display: contents;" >
+play_arrow
+</span>`;
+
+
 
 function setAry() {
-    debugger
+
     if (sumCard.value < 10 && sumCard.value != 0) {
         cards.innerHTML = `<h3  class = "col text-center align-middle"> you need to choose a min-number of ten </h3>`;
     }
@@ -47,20 +55,48 @@ function setAry() {
         console.log(_ary.toString());
         console.log(Math.floor(Math.random() * sumCard.value));
         sumCard.blur();
+        startTime();
     }
 }
+
+function startTime() {
+    setTimeout(() => {
+        setInterval(() => {
+            if (secnce <= 8) {
+                secnce++
+            } else {
+                secnce = 0;
+                if (minit <= 4) {
+                    minit++
+                } else {
+                    minit++
+                    minit = 0;
+                    ouers++;
+                }
+            }
+            document.getElementById("time").innerHTML = `${ouers}:${minit}${secnce}<span onClick = "stoPlay()"  class="material-icons m-2" style="font-size: medium; display: contents;" >
+            play_arrow
+            </span>`;
+        }, 1000);
+    }, 2 * 1000)
+}
+
+// function stoPlay() {
+//     if()
+    
+// }
 
 function display(e) {
 
     if (card1 && card2) {
 
     } else {
-        e.target.innerHTML = _ary[+e.target.id]; 
+        e.target.innerHTML = _ary[+e.target.id];
         e.target.className = "bek col-1 text-center align-middle";
         if (card1 === undefined) {
             card1 = e.target.innerHTML;
             id1 = e.target.id;
-        } else {
+        } else if (e.target.id !== id1) {
             card2 = e.target.innerHTML;
             steps++;
             let id2 = e.target.id;
@@ -72,7 +108,7 @@ function display(e) {
                     id1 = undefined;
                     card2 = undefined;
                     id2 = undefined;
-                    
+
                 }, 1 * 1000)
             } else {
                 card1 = undefined;
@@ -80,12 +116,12 @@ function display(e) {
                 card2 = undefined;
                 id2 = undefined;
 
-                finish += 2;
+                finish++;
                 console.log(sumCard.value)
-                if (finish == sumCard.value * 2) {
+                if (finish == sumCard.value) {
                     setTimeout(() => {
                         cards.innerHTML = `<h3  class = "col text-center align-middle"> game is finished succefully in ${steps} steps <br>
-                    for new game input a number for card's number above the page and press the botten   </h3>`
+                    for new game input a number for card's number above the page and press start game   </h3>`
                     }, 2 * 1000)
 
 
